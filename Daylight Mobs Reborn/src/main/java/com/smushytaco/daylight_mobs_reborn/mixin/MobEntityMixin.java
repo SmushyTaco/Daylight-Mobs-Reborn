08128f6a83724ada5fb5e541786098f33aea6063
SmushyTaco <smushytaco@gmail.com>
2021-05-01 15:22:26 -0700

    Add Config For Mod

--- Daylight Mobs Reborn/src/main/java/com/smushytaco/daylight_mobs_reborn/mixin/MobEntityMixin.java
+++ Daylight Mobs Reborn/src/main/java/com/smushytaco/daylight_mobs_reborn/mixin/MobEntityMixin.java
@@ -1,15 +1,15 @@
-package com.smushytaco.daylight_mobs_reborn.mixins;
-
+package com.smushytaco.daylight_mobs_reborn.mixin;
+import com.smushytaco.daylight_mobs_reborn.DaylightMobsReborn;
 import net.minecraft.entity.mob.MobEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 @Mixin(MobEntity.class)
 public abstract class MobEntityMixin {
     @Inject(method = "isAffectedByDaylight", at = @At("RETURN"), cancellable = true)
     public void hookDaylight(CallbackInfoReturnable<Boolean> ci) {
+        if (!DaylightMobsReborn.INSTANCE.getConfig().getMobsDontBurnDuringTheDay()) return;
         ci.setReturnValue(false);
     }
 }
