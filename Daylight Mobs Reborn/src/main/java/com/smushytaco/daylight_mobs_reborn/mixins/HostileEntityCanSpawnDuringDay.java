62d30bf1a75c23c4d3d430139d4e8a70c91dd326
Nikan Radan <github.com@alias.nikanradan.com>
2023-06-15 03:00:08 -0700

    1.20.1

--- Daylight Mobs Reborn/src/main/java/com/smushytaco/daylight_mobs_reborn/mixins/HostileEntityCanSpawnDuringDay.java
+++ Daylight Mobs Reborn/src/main/java/com/smushytaco/daylight_mobs_reborn/mixins/HostileEntityCanSpawnDuringDay.java
@@ -1,4 +1,5 @@
 package com.smushytaco.daylight_mobs_reborn.mixins;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.smushytaco.daylight_mobs_reborn.DaylightMobsReborn;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.util.math.BlockPos;
@@ -7,18 +8,12 @@ import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(HostileEntity.class)
 public abstract class HostileEntityCanSpawnDuringDay {
-    @Inject(method = "isSpawnDark", at = @At("RETURN"), cancellable = true)
-    private static void hookIsSpawnDark(ServerWorldAccess world, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
-        if (!DaylightMobsReborn.INSTANCE.getConfig().getHostileMobsSpawnDuringTheDay()) return;
-        cir.setReturnValue(world.getLightLevel(pos, 10) <= random.nextInt(8));
-    }
-    @Inject(method = "getPathfindingFavor", at = @At("RETURN"), cancellable = true)
-    public void hookGetPathfindingFavor(BlockPos pos, WorldView world, CallbackInfoReturnable<Float> cir) {
-        if (!DaylightMobsReborn.INSTANCE.getConfig().getHostileMobsSpawnDuringTheDay()) return;
-        cir.setReturnValue(1.0F);
-    }
+    @ModifyReturnValue(method = "isSpawnDark", at = @At("RETURN"))
+    @SuppressWarnings("unused")
+    private static boolean hookIsSpawnDark(boolean original, ServerWorldAccess world, BlockPos pos, Random random) { return !DaylightMobsReborn.INSTANCE.getConfig().getHostileMobsSpawnDuringTheDay() ? original : world.getLightLevel(pos, 10) <= random.nextInt(8); }
+    @ModifyReturnValue(method = "getPathfindingFavor", at = @At("RETURN"))
+    @SuppressWarnings("unused")
+    public float hookGetPathfindingFavor(float original, BlockPos pos, WorldView world) { return !DaylightMobsReborn.INSTANCE.getConfig().getHostileMobsSpawnDuringTheDay() ? original : 1.0F; }
 }
--- Daylight Mobs Reborn/src/main/java/com/smushytaco/daylight_mobs_reborn/mixins/MobEntityDoesntBurnDuringDay.java
+++ Daylight Mobs Reborn/src/main/java/com/smushytaco/daylight_mobs_reborn/mixins/MobEntityDoesntBurnDuringDay.java
@@ -1,17 +1,11 @@
 package com.smushytaco.daylight_mobs_reborn.mixins;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.smushytaco.daylight_mobs_reborn.DaylightMobsReborn;
-import com.smushytaco.daylight_mobs_reborn.configuration_support.ModConfiguration;
 import net.minecraft.entity.mob.MobEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(MobEntity.class)
 public abstract class MobEntityDoesntBurnDuringDay {
-    @Inject(method = "isAffectedByDaylight", at = @At("RETURN"), cancellable = true)
-    public void hookIsAffectedByDaylight(CallbackInfoReturnable<Boolean> cir) {
-        ModConfiguration config = DaylightMobsReborn.INSTANCE.getConfig();
-        if (!config.getMobsDontBurnDuringTheDay() && !config.getHostileMobsSpawnDuringTheDay()) return;
-        cir.setReturnValue(false);
-    }
+    @ModifyReturnValue(method = "isAffectedByDaylight", at = @At("RETURN"))
+    public boolean hookIsAffectedByDaylight(boolean original) { return !DaylightMobsReborn.INSTANCE.getConfig().getMobsDontBurnDuringTheDay() && !DaylightMobsReborn.INSTANCE.getConfig().getHostileMobsSpawnDuringTheDay() && original; }
 }
